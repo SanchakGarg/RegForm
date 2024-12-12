f89dcd0c81b9a69d4f7e0d9cfb76e03469a01735
SanchakGarg <sanchakgargss@gmail.com>
2024-12-12 13:54:56 +0530

    sign in ui

---
+++ app/authentication/SignUp/page.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+
+export default function SignUp() {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>sign up</CardTitle>
+                {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" type="email" placeholder="email" />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" type="password" placeholder="password" />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-end">
+                <Button>Sign In</Button>
+            </CardFooter>
+        </Card>
+    )
+}

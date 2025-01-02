03d3d12423c2769d6ea5ae28350a8de31170c686
Hosting <hosting@server.agneepath.co.in>
2025-01-02 15:35:21 +0000

    increase size of sizebar trigger

--- app/(authentication)/layout.tsx
+++ app/(authentication)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center justify-items-center">
         <Image
           className=""
